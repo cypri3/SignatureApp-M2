a32d5abf65f4dd075075e0d135483dcf10088644
cypri <cypri3.jules@free.fr>
2024-11-24 17:08:17 +0100

    crypto add-on

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 
  - Implémentation de MD5 en python : https://github.com/timvandermeij/md5.py/blob/master/md5.py
  - Psueod code de MD5 : https://en.wikipedia.org/wiki/MD5#Pseudocode
+
+ Pour compiler : 
+ javac -cp "lib/*" -d bin src/Main.java
+ Pour executer :
+ java -cp "lib/*:bin" Main
+ Pour trouver l'emplacement d'une classe :
+ jar tf lib/jpbc-plaf-2.0.0.jar | grep PairingFactory
+ Pour les a.properties :
+ https://github.com/chrizchow/JPBC-ABS2/blob/master/a.properties
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.jpbc.Pairing;
+import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, BLS signature project!");
+        try {
+            // Charger les paramètres de courbe depuis un fichier
+            Pairing pairing = PairingFactory.getPairing("src/a.properties");
+
+            // Générer les paramètres du système
+            Element g = pairing.getG1().newRandomElement().getImmutable();
+
+            // Générer une clé secrète
+            Element x = pairing.getZr().newRandomElement();
+
+            // Générer la clé publique correspondante
+            Element pk = g.powZn(x);
+
+            System.out.println("Clé secrète : " + x);
+            System.out.println("Clé publique : " + pk);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
