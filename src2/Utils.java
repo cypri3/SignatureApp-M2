82e40699b7541485a8de63eeb2b22ef60aabbc3d
Cl0miA <cloe.flageul@gmail.com>
2024-12-10 16:25:47 +0100

    Hexa

--- src2/Utils.java
+++ src2/Utils.java
@@ -1,5 +1,16 @@
 package src2;
 
-public class Utils {
+import java.math.BigInteger;
 
+public class Utils {
+    static BigInteger hexToBigInt(String stringHex){
+        BigInteger integerHex = new BigInteger(stringHex ,16);
+        return integerHex;
+        }
+
+    static String bigIntToHex(BigInteger bigInteger){
+        int integerHex = bigInteger.intValue();
+        String stringHex=Integer.toHexString(integerHex);
+        return stringHex;
+        }
 }
