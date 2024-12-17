96acf892e2bf282fce01b2d26678f919567e4ccc
albanprx <perreauxalban@gmail.com>
2024-12-17 10:01:07 +0100

    update v3 PDFdata

--- src/PDFdata.java
+++ src/PDFdata.java
@@ -8,7 +8,10 @@ import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 public class PDFdata {
     private File file;
 
-    public PDFdata(String filePath) {
+    public PDFdata() {
+    }
+
+    public void setPathFile(String filePath) {
         this.file = new File(filePath);
     }
 
@@ -67,7 +70,7 @@ public class PDFdata {
         }
     }
 
-        public static byte[] readPDFAsBytes(File file) throws IOException {
+    public static byte[] readPDFAsBytes(File file) throws IOException {
         try (FileInputStream fis = new FileInputStream(file)) {
             byte[] fileBytes = new byte[(int) file.length()];
             fis.read(fileBytes);
